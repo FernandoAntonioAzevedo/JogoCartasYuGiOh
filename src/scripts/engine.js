1ba08181de3c89858acdc6c92024d96ee7317f51
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-10-08 21:25:49 -0300

    inserindo cursores e música de fundo durante os duelos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -203,7 +203,8 @@ function init() {
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
     
-    
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 }
 
 init();
